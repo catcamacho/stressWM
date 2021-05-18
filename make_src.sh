2f95dea0e6f51bf4e5f8a4befeaaa2f4b7f67a6d
Cat <camachoc@wustl.edu>
2021-05-17 20:50:01 -0500

    finalize data analysis

--- make_src.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/csh
-
-foreach sub ($argv)
-
-	mkdir /data/perlman/moochie/user_data/CamachoCat/EmoGrow/dMRI_proc/proc/preprocessing/dsi_studio/${sub}
-	dsi_studio --action=src --source=/data/perlman/moochie/user_data/CamachoCat/EmoGrow/dMRI_proc/proc/preprocessing/unwarped_niftis/${sub}/dmri_unwarped.nii.gz --b_table=/data/perlman/moochie/user_data/CamachoCat/EmoGrow/dMRI_proc/proc/preprocessing/unwarped_niftis/${sub}/sub-${sub}_btable.txt --output=/data/perlman/moochie/user_data/CamachoCat/EmoGrow/dMRI_proc/proc/preprocessing/dsi_studio/${sub}/sub-${sub}_dMRI_98dir_unwarped.src
-
-end
